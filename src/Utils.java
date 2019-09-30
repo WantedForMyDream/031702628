937720043012229a39009a05700a0cebed840cb1
YSong <976806477@qq.com>
2019-09-30 21:20:15 +0800

    update

--- src/Utils.java
+++ src/Utils.java
@@ -1,10 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
     public static String loadJSON (String url) {
@@ -32,5 +33,55 @@ public class Utils {
         BigDecimal bd = new BigDecimal(num);
         num = bd.setScale(6, BigDecimal.ROUND_HALF_UP).doubleValue();
         return num;
+    }
+
+    public static void fileWrite(String path,String string){
+        BufferedWriter bw = null;
+        File file = new File(path);
+        try {
+            if (file.exists()) {
+                file.delete();
+                file.createNewFile();
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
+            bw = new BufferedWriter(outputStreamWriter);
+            bw.write(string);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bw != null) {
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static List fileRead(String path){
+        List<String> list = new ArrayList<>();
+        BufferedReader reader = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+            reader = new BufferedReader(inputStreamReader);
+            String s=null;
+            while ((s = reader.readLine()) != null) {//逐行读取文件内容，不读取换行符和末尾的空格
+                list.add(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return list;
     }
 }
